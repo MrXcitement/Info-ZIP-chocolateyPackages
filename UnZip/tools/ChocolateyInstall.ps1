b3aedc43a89c07fa28f44cc5cacee4e1bde6f604
Mike Barker <mike@thebarkers.com>
2012-09-14 15:11:30 -0600

    Fix invalid package name and url.

--- UnZip/tools/ChocolateyInstall.ps1
+++ UnZip/tools/ChocolateyInstall.ps1
@@ -1,2 +1,1 @@
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-Install-ChocolateyZipFile 'Zip' 'ftp://ftp.info-zip.org/pub/infozip/win32/unz600xn.exe' "$scriptPath"
+Install-ChocolateyZipPackage 'UnZip' 'ftp://ftp.info-zip.org/pub/infozip/win32/unz600xn.exe' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
